b65ab5c1a8bfdcee8112a7eafa2570ee23739b3e
yamsellem <amsellem.yves@gmail.com>
2024-06-21 10:13:56 +0200

    Fix: Domain constructor clones input data (like Mapper)

--- test/domain.test.js
+++ test/domain.test.js
@@ -3,6 +3,12 @@ const {expect} = require('chai');
 const Domain = require('../lib/Domain');
 
 class Thing extends Domain {
+    name;
+    constructor(data) {
+        super();
+        Domain.assign(this, data);
+    }
+
     get props() {
         return {
             name: String
@@ -11,7 +17,9 @@ class Thing extends Domain {
 }
 
 class Box extends Domain {
-    name;
+    size;
+    thing;
+
     constructor(data) {
         super();
         Domain.assign(this, data);
@@ -67,5 +75,45 @@ describe('domain', () => {
         Domain.assign(thing, {name: name});
 
         expect(thing.name).to.equal(name);
+    });
+
+    class Bookcase extends Domain {
+        size;
+        things;
+        
+        constructor(data) {
+            super();
+            Domain.assign(this, data);
+        }
+
+        get props() {
+            return {
+                size: String,
+                things: [Thing]
+            };
+        }
+    }
+
+    it('domain creation (cloning props)', () => {
+        const name = 'Shoes';
+        const json = {size: 40, thing: new Thing({name})};
+        const box = new Box(json);
+        const duplicate = new Box(json);
+
+        box.thing.name = '';
+
+        expect(box.thing.name).to.equal('');
+        expect(duplicate.thing.name).to.equal('Shoes');
+    });
+
+    it('domain creation (cloning array props)', () => {
+        const json = {size: 'Medium', things: [{name: 'Shoes'}, {name: 'Shirt'}]};
+        const bookcase = new Bookcase(json);
+        const duplicate = new Bookcase(json);
+
+        bookcase.things[0].name = '';
+
+        expect(bookcase.things[0].name).to.equal('');
+        expect(duplicate.things[0].name).to.equal('Shoes');
     });
 });
